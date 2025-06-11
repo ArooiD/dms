25aea133826cf77be820f2b95d9d522777627490
macbook <deniss.komissarov@gmail.com>
2025-06-11 14:41:35 +0300

    f

---
+++ core/storage/src/main/java/ru/mirea/designvault/storage/model/Project.java
@@ -0,0 +1,21 @@
+package ru.mirea.designvault.storage.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(schema = "public", name = "project")
+public class Project {
+    @Id
+    private UUID pid;
+    private UUID uid;
+    private Instant created;
+    private Instant modified;
+    private String access;
+}
