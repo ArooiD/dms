7e9b3f20fa3b940553fd1151abdda1bff445ad57
macbook <deniss.komissarov@gmail.com>
2025-06-11 14:31:35 +0300

    f

--- core/gateway/src/main/java/ru/mirea/designvault/gateway/controller/ProjectController.java
+++ core/gateway/src/main/java/ru/mirea/designvault/gateway/controller/ProjectController.java
@@ -3,12 +3,15 @@ package ru.mirea.designvault.gateway.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PactController {
+@RequestMapping("project")
+public class ProjectController {
+
     @GetMapping()
-    public Object getTest(@AuthenticationPrincipal Jwt token) {
+    public Object getAllProjects(@AuthenticationPrincipal Jwt token) {
         return token;
     }
 }
