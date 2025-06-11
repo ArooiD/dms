bcadacef88d854dab62c52d7874f53ddf94278ed
macbook <deniss.komissarov@gmail.com>
2025-06-11 14:38:07 +0300

    f

--- core/gateway/src/main/java/ru/mirea/designvault/gateway/dto/ResponseDto.java
+++ core/gateway/src/main/java/ru/mirea/designvault/gateway/dto/ResponseDto.java
@@ -3,11 +3,13 @@ package ru.mirea.designvault.gateway.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Builder
 public class ResponseDto {
     private int count;
-    private List<ResultItemDto> resultSet;
+    @Builder.Default
+    private List<ResultItemDto> resultSet = new ArrayList<>();
 }
