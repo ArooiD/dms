0cb731659151921655674e307705ad70bb9a5132
macbook <deniss.komissarov@gmail.com>
2025-06-11 18:44:50 +0300

    f

--- core/web/src/pages/LoginPage/LoginPage.jsx
+++ core/web/src/pages/LoginPage/LoginPage.jsx
@@ -28,7 +28,8 @@ const LoginPage = observer(() => {
 
     const performLogin = (values) => {
         setStatus('pending')
-        fetch(`${import.meta.env.VITE_DOMAIN}/api/auth/login`, {
+        const externalHost = import.meta.env.VITE_DOMAIN;
+        fetch(`/api/auth/login`, {
             method: 'POST',
             body: JSON.stringify(values),
             headers: {
