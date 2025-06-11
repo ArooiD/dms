a1691bf1b6613471f00d6ded010f2a25d1ede12a
macbook <deniss.komissarov@gmail.com>
2025-06-11 17:58:26 +0300

    f

--- core/storage/src/main/java/ru/mirea/designvault/storage/controller/ProjectController.java
+++ core/storage/src/main/java/ru/mirea/designvault/storage/controller/ProjectController.java
@@ -58,4 +58,8 @@ public class ProjectController {
 
         return new Document();
     }
+
+    
+
+
 }
--- core/storage/src/main/java/ru/mirea/designvault/storage/model/Project.java
+++ core/storage/src/main/java/ru/mirea/designvault/storage/model/Project.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Table(schema = "public", name = "project")
 public class Project {
     @Id
-    @Column(name = "pid", unique = true)
+    @Column(name = "pid")
     private UUID pid;
     @Id
     @Column(name = "uid")
