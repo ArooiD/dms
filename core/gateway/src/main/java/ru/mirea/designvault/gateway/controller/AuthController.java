e689a915c479e6e9c04cdab410060cf8e6d256b8
macbook <deniss.komissarov@gmail.com>
2025-06-11 13:16:36 +0300

    f

--- core/gateway/src/main/java/ru/mirea/designvault/gateway/controller/AuthController.java
+++ core/gateway/src/main/java/ru/mirea/designvault/gateway/controller/AuthController.java
@@ -1,7 +1,25 @@
 package ru.mirea.designvault.gateway.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.mirea.designvault.gateway.dto.AuthDto;
+import ru.mirea.designvault.gateway.service.AuthService;
+
+import java.util.Map;
 
 @RestController
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map> login(@RequestBody AuthDto dto) {
+        return ResponseEntity.ok(authService.getToken(dto));
+    }
 }
---
+++ core/gateway/src/main/java/ru/mirea/designvault/gateway/dto/AuthDto.java
@@ -0,0 +1,9 @@
+package ru.mirea.designvault.gateway.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthDto {
+    private String username;
+    private String password;
+}
