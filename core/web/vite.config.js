c62df8d5db4b75cab9659056cb27f37bd3f8316d
macbook <deniss.komissarov@gmail.com>
2025-06-11 12:19:37 +0300

    f

--- core/web/vite.config.js
+++ core/web/vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: true,
-        allowedHosts: ['core.web'],
+        allowedHosts: 'all',
+        // port: 3000,
+        hot: true,
     },
 })
