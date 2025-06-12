d272d5e9c7bd1332ff4026e6636b657538788dfb
macbook <deniss.komissarov@gmail.com>
2025-06-12 19:19:01 +0300

    Work wit file

--- core/storage/src/main/java/ru/mirea/designvault/storage/repository/DocumentChunkRepository.java
+++ core/storage/src/main/java/ru/mirea/designvault/storage/repository/DocumentChunkRepository.java
@@ -18,7 +18,7 @@ public interface DocumentChunkRepository extends CrudRepository<DocumentChunk, D
 
     @Transactional
     @Modifying
-    @Query(value = "INSERT INTO document_chunk (pid, did, cid, content, embedding) VALUES (:pid, :did, :cid, :content, :embedding::vector(384))", nativeQuery = true)
+    @Query(value = "INSERT INTO document_chunk (pid, did, cid, content, embedding) VALUES (:pid, :did, :cid, :content, :embedding)", nativeQuery = true)
     void insertChunk(@Param("pid") UUID pid,
                      @Param("did") UUID did,
                      @Param("cid") int cid,
