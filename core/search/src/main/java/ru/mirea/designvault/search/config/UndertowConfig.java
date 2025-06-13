234c08e93db1cb8ea523b8912242c914dd942f08
macbook <deniss.komissarov@gmail.com>
2025-06-13 11:22:14 +0300

    Work wit file

---
+++ core/search/src/main/java/ru/mirea/designvault/search/config/UndertowConfig.java
@@ -0,0 +1,23 @@
+package ru.mirea.designvault.search.config;
+
+import io.undertow.UndertowOptions;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UndertowConfig implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+    @Override
+    public void customize(UndertowServletWebServerFactory factory) {
+        factory.addBuilderCustomizers(builder -> {
+            builder.setIoThreads(8);
+            builder.setWorkerThreads(32);
+            builder.setBufferSize(16384);
+            builder.setDirectBuffers(true);
+            builder.setServerOption(UndertowOptions.ALWAYS_SET_KEEP_ALIVE, true);
+        });
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> {
+            deploymentInfo.setEagerFilterInit(true);
+        });
+    }
+}
