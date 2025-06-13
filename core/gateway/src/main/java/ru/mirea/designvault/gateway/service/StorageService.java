eafb624d3b431655ee71a16a138047b8bdbe85b6
macbook <deniss.komissarov@gmail.com>
2025-06-13 15:32:10 +0300

    Work wit file

--- core/gateway/src/main/java/ru/mirea/designvault/gateway/service/StorageService.java
+++ core/gateway/src/main/java/ru/mirea/designvault/gateway/service/StorageService.java
@@ -28,8 +28,8 @@ public class StorageService {
         return result;
     }
 
-    public UUID resolveDocumentSlug(String documentSlug) {
-        ResponseEntity<UUID> response = client.getForEntity("/documents/" + documentSlug + "/did", UUID.class);
+    public UUID resolveDocumentSlug(UUID pid, String documentSlug) {
+        ResponseEntity<UUID> response = client.getForEntity("/projects/" + pid + "/documents/" + documentSlug + "/did", UUID.class);
         UUID result = response.getBody();
         if (result == null) {
             throw new RuntimeException("Document UUID not found for slug: " + documentSlug);
@@ -38,8 +38,8 @@ public class StorageService {
     }
 
     public ResponseEntity<Resource> getFileObjectVersion(String slug, String name, Integer ver) {
-        UUID pid = UUID.fromString("84055427-00ea-41e0-800a-e3e31f8acefc");
-        UUID did = UUID.fromString("1eef8b77-0e78-4f34-b45c-95d3017b4239");
+        UUID pid = resolveProjectSlug(slug);
+        UUID did = resolveDocumentSlug(pid, name);
         String url;
         Object[] uriVariables;
         if (ver == null) {
@@ -69,8 +69,8 @@ public class StorageService {
     }
 
     public ResponseEntity<Resource> getPreviewObjectVersion(String slug, String name, Integer ver) {
-        UUID pid = UUID.fromString("84055427-00ea-41e0-800a-e3e31f8acefc");
-        UUID did = UUID.fromString("1eef8b77-0e78-4f34-b45c-95d3017b4239");
+        UUID pid = resolveProjectSlug(slug);
+        UUID did = resolveDocumentSlug(pid, name);
         String url;
         Object[] uriVariables;
         if (ver == null) {
@@ -100,11 +100,21 @@ public class StorageService {
     }
 
     public Object addFileObjectVersion(String slug, UUID uid, MultipartFile file) {
+        UUID pid = resolveProjectSlug(slug);
+
+
+
+
+
+
+
 
         return null;
     }
 
     public Object addFileObjectVersion(String slug, String name, UUID uid, MultipartFile file) {
+        UUID pid = resolveProjectSlug(slug);
+        UUID did = resolveDocumentSlug(pid, name);
         return null;
     }
 }
