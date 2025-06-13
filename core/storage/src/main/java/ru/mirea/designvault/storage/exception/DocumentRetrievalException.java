92bc93cdd704ac586f80922ebcd2d6c7cb7ca937
macbook <deniss.komissarov@gmail.com>
2025-06-13 13:38:47 +0300

    Work wit file

--- core/storage/src/main/java/ru/mirea/designvault/storage/exception/DocumentRetrievalException.java
+++ core/storage/src/main/java/ru/mirea/designvault/storage/exception/DocumentRetrievalException.java
@@ -3,5 +3,6 @@ package ru.mirea.designvault.storage.exception;
 public class DocumentRetrievalException extends RuntimeException {
     public DocumentRetrievalException(String message, Throwable cause) {
         super(message, cause);
+
     }
 }
