9cb620a1ce6ff5a5917058a0c37c2794c1289dab
macbook <deniss.komissarov@gmail.com>
2025-06-13 15:47:08 +0300

    Work wit file

--- core/gateway/src/main/java/ru/mirea/designvault/gateway/service/StorageService.java
+++ core/gateway/src/main/java/ru/mirea/designvault/gateway/service/StorageService.java
@@ -1,12 +1,16 @@
 package ru.mirea.designvault.gateway.service;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Service
@@ -99,22 +103,42 @@ public class StorageService {
         return new ResponseEntity<>(response.getBody(), headers, HttpStatus.OK);
     }
 
-    public Object addFileObjectVersion(String slug, UUID uid, MultipartFile file) {
+    public Object addFileObjectVersion(String slug, UUID uid, MultipartFile file) throws IOException {
         UUID pid = resolveProjectSlug(slug);
-
-
-
-
-
-
-
-
-        return null;
+        String url = "/files/" + pid + "/";
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        ByteArrayResource fileAsResource = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        };
+        body.add("file", fileAsResource);
+        body.add("uid", uid.toString());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<Object> response = client.postForEntity(url, requestEntity, Object.class);
+        return response.getBody();
     }
 
-    public Object addFileObjectVersion(String slug, String name, UUID uid, MultipartFile file) {
+    public Object addFileObjectVersion(String slug, String name, UUID uid, MultipartFile file) throws IOException {
         UUID pid = resolveProjectSlug(slug);
         UUID did = resolveDocumentSlug(pid, name);
-        return null;
+        String url = "/files/" + pid + "/" + did;
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        ByteArrayResource fileAsResource = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        };
+        body.add("file", fileAsResource);
+        body.add("uid", uid.toString());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<Object> response = client.postForEntity(url, requestEntity, Object.class);
+        return response.getBody();
     }
 }
