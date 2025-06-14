e57f919d65fc2aa22ed03e56399eba019cbd5107
Sijango <klimkin77@gmail.com>
2025-06-14 17:38:34 +0300

    fix uuid

--- core/ocr/app/api/health.py
+++ core/ocr/app/api/health.py
@@ -73,8 +73,9 @@ async def upload_file(
     pid: str = Form(..., description="pidr"),
     did: str = Form(..., description="did inside"),
     ver: int = Form(..., description="verMut"),
-    file: UploadFile = File(...)):
-    time_parse = time.time()
+    file: UploadFile = File(...)
+):
+
     if not file.filename:
         raise HTTPException(status_code=400, detail="Empty file")
 
@@ -86,12 +87,15 @@ async def upload_file(
 
     if file_extension in [".docx"] and mime == "text/html":
         mime = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    pid = uuid.UUID(pid)
+    did = uuid.UUID(did)
+    print(f"pid:{pid} did:{did}")
 
     try:
         text = await parse_file(io.BytesIO(contents), mime)
         data = {
-            "pid": uuid.UUID(pid),
-            "did": uuid.UUID(did),
+            "pid": pid,
+            "did": did,
             "ver": ver,
             "frags": parse_text(text, _split_symbol=" . \n") if file_extension == ".pdf" else parse_text(text, _split_symbol="\n"),
         }
@@ -123,3 +127,7 @@ def parse_text(text: str, _split_symbol):
 
 def is_empty(line: str):
     return not line or line.strip() == ""
+
+
+if __name__ == '__main__':
+    print(uuid.UUID("123e4567-e89b-12d3-a456-426655440000"))
