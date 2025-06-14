f24850ccd81201d519ba85faf49580117c0561c6
Sijango <klimkin77@gmail.com>
2025-06-14 17:03:26 +0300

    fix connection db & rabbitmq

--- core/ocr/app/infra/messaging.py
+++ core/ocr/app/infra/messaging.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_rabbitmq_connection():
-    rabbitmq_host = os.getenv("RABBITMQ_HOST", "dv.istokmw.tech")
+    rabbitmq_host = os.getenv("RABBITMQ_HOST", "infra.rabbitmq")
     rabbitmq_port = int(os.getenv("RABBITMQ_PORT", 5672))
     rabbitmq_user = os.getenv("RABBITMQ_USER", "guest")
     rabbitmq_password = os.getenv("RABBITMQ_PASSWORD", "guest")
