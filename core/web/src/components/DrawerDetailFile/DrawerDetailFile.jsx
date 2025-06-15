f77433925491ea4386b2f0ca1425cb7a4338dc84
macbook <deniss.komissarov@gmail.com>
2025-06-16 01:31:26 +0300

    minor

--- core/web/src/components/DrawerDetailFile/DrawerDetailFile.jsx
+++ core/web/src/components/DrawerDetailFile/DrawerDetailFile.jsx
@@ -67,7 +67,7 @@ const DrawerDetailFile = observer(({setOpenModalPreviewFile, callback_open, call
     const getFile = () => {
         const externalHost = import.meta.env.VITE_DOMAIN || "";
 
-        setPreviewFileUrl(`${externalHost}/api/projects/${project_id}/preview/${selectedFile.slug}/${selectedFile.version}`)
+        setPreviewFileUrl(`${externalHost}/api/projects/${project_id}/preview/${selectedFile.slug}/${selectedVersion}`)
         callback_close()
         setOpenModalPreviewFile(true)
         // fetch(`${externalHost}/api/projects/${project_id}/preview/${selectedFile.slug}`, {
