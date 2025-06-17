3396678bb710a5de935768a703a30f03ad5d8a31
macbook <deniss.komissarov@gmail.com>
2025-06-17 23:39:57 +0300

    minor

--- core/search/src/main/java/ru/mirea/designvault/search/repository/DocumentTagRepository.java
+++ core/search/src/main/java/ru/mirea/designvault/search/repository/DocumentTagRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.mirea.designvault.search.key.DocumentTagId;
+import ru.mirea.designvault.search.key.DocumentVersionId;
 import ru.mirea.designvault.search.model.DocumentVersionTag;
 
 import java.util.List;
@@ -12,5 +13,5 @@ import java.util.List;
 @Repository
 public interface DocumentTagRepository extends JpaRepository<DocumentVersionTag, DocumentTagId> {
     @Query(nativeQuery = true, value = "select distinct t.pid, t.did, t.ver from document_version_tag t where t.name in :tags")
-    List<DocumentTagId> findIdByTag(@Param("tags") List<String> tags);
+    List<DocumentVersionId> findIdByTag(@Param("tags") List<String> tags);
 }
